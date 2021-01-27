748e8a805a2327172839433fc4d8cc5a9902a595
Kennywd <78007165+Kennywd@users.noreply.github.com>
2021-01-27 12:28:40 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************************
+* Hello World 
+***********************************************************
+* Wutharo Duong
+* 01/27/21
+* 255 and 003
+**********************************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("Wutharo Duong");
+	System.out.println("Lab 1");
+	System.out.println("Introduction to the Course for Mac");
+	System.out.println("01/27/21");
+	System.out.println("255 003");
+	System.out.println();
+	}
+}
